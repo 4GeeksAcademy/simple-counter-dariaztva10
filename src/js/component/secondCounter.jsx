7fdb65f5134dd130e429b1e389e0b6f9cd7cfda2
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-08-04 17:18:16 +0000

    New commit

--- src/js/component/secondCounter.jsx
+++ src/js/component/secondCounter.jsx
@@ -6,15 +6,15 @@ const SecondCounter = (props) => {
 	return (
 		
 				<div className="containar_principal row">
-					<div className="container_icon col-lg-2 col-sm-12">
+					<div className="container_icon col-9 col-lg-2 col-sm-9">
 					<i class="far fa-clock"></i>
 					</div>
-					<div className="seis col">{props.seis % 10}</div>
-					<div className="cinco col">{props.cinco % 10}</div>
-					<div className="cuatro col">{props.cuatro % 10}</div>
-					<div className="tres col">{props.tres % 10}</div>
-					<div className="dos col">{props.dos % 10}</div>
-					<div className="uno col">{props.unos % 10}</div>
+					<div className="seis col-4 col-lg-1 col-sm-4">{props.seis % 10}</div>
+					<div className="cinco col-4 col-lg-1 col-sm-4">{props.cinco % 10}</div>
+					<div className="cuatro col-4 col-lg-1 col-sm-4">{props.cuatro % 10}</div>
+					<div className="tres col-4 col-lg-1 col-sm-4">{props.tres % 10}</div>
+					<div className="dos col-4 col-lg-1 col-sm-4">{props.dos % 10}</div>
+					<div className="uno col-4 col-lg-1 col-sm-4">{props.unos % 10}</div>
 				</div>
 			
 		
